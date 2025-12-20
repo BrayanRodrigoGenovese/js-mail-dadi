df9fc2e9ae5a4660b6c280ac8aa466831a33db64
Brayan <bittolo.8bit@gmail.com>
2025-12-20 09:45:22 +0100

    Enhance logging for generated random numbers

--- Dadi/script.js
+++ Dadi/script.js
@@ -1,5 +1,5 @@
+// random generator
 const aiRandomNumber = Math.round(Math.random() * (6 - 1) + 1);
 const userRandomNumber = Math.round(Math.random() * (6 - 1) + 1);
-
-console.log(aiRandomNumber);
-console.log(userRandomNumber);
+console.log(`aiRandomNumber = ${aiRandomNumber}`);
+console.log(`userRandomNumber = ${userRandomNumber}`);
