668ccd144eae9f2e72b2f2b28d9219b539d13066
Brayan <bittolo.8bit@gmail.com>
2025-12-20 09:46:33 +0100

    Implement winner comparison logic

--- Dadi/script.js
+++ Dadi/script.js
@@ -3,3 +3,12 @@ const aiRandomNumber = Math.round(Math.random() * (6 - 1) + 1);
 const userRandomNumber = Math.round(Math.random() * (6 - 1) + 1);
 console.log(`aiRandomNumber = ${aiRandomNumber}`);
 console.log(`userRandomNumber = ${userRandomNumber}`);
+
+// comparison logic
+if (aiRandomNumber > userRandomNumber) {
+  console.log("Computer wins!");
+} else if (aiRandomNumber < userRandomNumber) {
+  console.log("You won!");
+} else {
+  console.log("Tie!");
+}
