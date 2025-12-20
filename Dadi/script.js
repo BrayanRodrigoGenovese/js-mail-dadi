f35b3ea02d6bf6f6e1572402a8cd88a45bb812a7
Brayan <bittolo.8bit@gmail.com>
2025-12-20 09:30:25 +0100

    Add random number generation for computer and user

---
+++ Dadi/script.js
@@ -0,0 +1,5 @@
+const aiRandomNumber = Math.round(Math.random() * (6 - 1) + 1);
+const userRandomNumber = Math.round(Math.random() * (6 - 1) + 1);
+
+console.log(aiRandomNumber);
+console.log(userRandomNumber);
