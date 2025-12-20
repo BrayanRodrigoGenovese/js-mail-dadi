ee361a774ec6795d2b9cde1e69522dffd8a7c677
Brayan <bittolo.8bit@gmail.com>
2025-12-20 09:13:30 +0100

    better refactoring

--- Mail/script.js
+++ Mail/script.js
@@ -6,6 +6,20 @@ const guestEmails = [
   "diego.t@boolean.com",
 ];
 console.table(guestEmails);
+
 // Inserimento della email da parte dell'utente
 const userEmail = prompt("Inserisci la tua email").toLowerCase();
 console.log(`userEmail = ${userEmail}`);
+
+// Controllo della email
+let isInvited = false;
+for (let i = 0; i < guestEmails.length && !isInvited; i++) {
+  const guest = guestEmails[i];
+  if (guest === userEmail) {
+    isInvited = true;
+  }
+}
+console.log(`isInvited = ${isInvited}`);
+
+// Esito del controllo
+alert(`${isInvited ? "" : "non "}sei nella lista`);
